c9e177470ff5cc7b40053f6f1bf035390ef8e174
Wellington Mazoni <wmazoni@protonmail.com>
2022-01-24 15:51:15 -0300

    Ajustando construtor para inicializar atributos da classe User

--- src/main/kotlin/br/com/wmazoni/demokotlinmongodb/models/embedded/Author.kt
+++ src/main/kotlin/br/com/wmazoni/demokotlinmongodb/models/embedded/Author.kt
@@ -1,8 +1,13 @@
 package br.com.wmazoni.demokotlinmongodb.models.embedded
 
 import br.com.wmazoni.demokotlinmongodb.models.entities.User
-import org.springframework.data.annotation.Id
 
-class Author(@Id var id: String?, var name: String) {
+class Author(entity: User) {
+    val id: String
+    val name: String
 
+    init {
+        id = entity.id!!
+        name = entity.name
+    }
 }
