7612658b5568418e7e44d509e551bea89bdb1350
Wellington Mazoni <wmazoni@protonmail.com>
2022-01-24 15:51:34 -0300

    adicionando data

--- src/main/kotlin/br/com/wmazoni/demokotlinmongodb/models/embedded/Comment.kt
+++ src/main/kotlin/br/com/wmazoni/demokotlinmongodb/models/embedded/Comment.kt
@@ -1,9 +1,6 @@
 package br.com.wmazoni.demokotlinmongodb.models.embedded
 
-import br.com.wmazoni.demokotlinmongodb.models.entities.User
-import org.springframework.data.annotation.Id
 import java.time.Instant
 
-class Comment(var text: String, var moment: Instant, var author:Author) {
-
+data class Comment(var text: String, var moment: Instant, var author:Author) {
 }
