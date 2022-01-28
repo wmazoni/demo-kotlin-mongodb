2b9150e6a43217f18c38cab140bf3b64155f6af2
Wellington Mazoni <wmazoni@protonmail.com>
2022-01-27 21:35:18 -0300

    classe resource User

--- src/main/kotlin/br/com/wmazoni/demokotlinmongodb/resources/UserResource.kt
+++ src/main/kotlin/br/com/wmazoni/demokotlinmongodb/resources/UserResource.kt
@@ -1,21 +1,27 @@
 package br.com.wmazoni.demokotlinmongodb.resources
 
+import br.com.wmazoni.demokotlinmongodb.dto.UserDTO
 import br.com.wmazoni.demokotlinmongodb.models.entities.User
+import br.com.wmazoni.demokotlinmongodb.services.UserService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/users")
-class UserResource {
+class UserResource(val userService: UserService) {
 
     @GetMapping
-    fun findAll(): ResponseEntity<MutableList<User>> {
-        val maria = User("1", "Maria Brown", "maria@gmail.com")
-        val alex = User("2", "Maria Silva", "maria@gmail.com")
-        val list: MutableList<User> = mutableListOf()
-        list.addAll(arrayListOf(maria, alex))
-        return ResponseEntity.ok(list)
+    fun findAll(): ResponseEntity<List<UserDTO>> {
+        val list: List<UserDTO> = userService.findAll()
+        return ResponseEntity.ok().body(list)
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: String): ResponseEntity<UserDTO> {
+        val obj: UserDTO = userService.findById(id)
+        return ResponseEntity.ok().body(obj)
     }
 }
