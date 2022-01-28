c67ff55122cea4613cc0e6b93699c714c4a2282f
Wellington Mazoni <wmazoni@protonmail.com>
2022-01-28 20:53:11 -0300

    ajuste para inserção de dados usando dto

--- src/main/kotlin/br/com/wmazoni/demokotlinmongodb/dto/UserDTO.kt
+++ src/main/kotlin/br/com/wmazoni/demokotlinmongodb/dto/UserDTO.kt
@@ -2,14 +2,8 @@ package br.com.wmazoni.demokotlinmongodb.dto
 
 import br.com.wmazoni.demokotlinmongodb.models.entities.User
 
-class UserDTO(entity: User) {
-    val id: String
-    val name: String
-    val email: String
-
-    init {
-        id = entity.id!!
-        name = entity.name
-        email = entity.email
+class UserDTO(var id: String? = null, var name: String = "", var email: String = "") {
+    constructor(entity: User) : this(id = entity.id, name = entity.name, email = entity.email) {
     }
+
 }
--- src/main/kotlin/br/com/wmazoni/demokotlinmongodb/models/entities/User.kt
+++ src/main/kotlin/br/com/wmazoni/demokotlinmongodb/models/entities/User.kt
@@ -7,9 +7,10 @@ import org.springframework.data.mongodb.core.mapping.Document
 @Document(collection = "user")
 data class User(
     @Id var id: String?,
-    val name: String,
-    val email: String,
-    @DBRef(lazy = true) var posts: ArrayList<Post>? = ArrayList()
+    var name: String,
+    var email: String,
+    @DBRef(lazy = true) var posts: List<Post>? = ArrayList()
 ) {
+    constructor() : this(null, "", "", emptyList())
 
 }
